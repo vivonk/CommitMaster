6ba7ba4242ffea2c70474b5fcce881ca947da3e1
vivonk <nirmalsarswat400@gmail.com>
2017-03-08 02:09:19 +0530

    ActivitiesFilterWeek1PointerQuestion3.java

--- commands.sh
+++ commands.sh
@@ -720,10 +720,9 @@ do
     		echo "${array_code[$i]} ${array_code[$j]}">"ActivitiesFilterWeek1${array_name[$i]}Question${j}.java"
 		git add .
 		git commit --date=$j/$i/2017 -m "ActivitiesFilterWeek1${array_name[$i]}Question${j}.java"
-		git push 
 	done
 done
-
+git push --all
 
 
 
